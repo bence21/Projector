4ec0a7c90d31dd04765e9908860a273062d70dcd
Bako Bencze <bence@gineri.com>
2024-05-01 21:39:41 +0300

    Credentials file

---
+++ projector-desktop/src/main/java/projector/Credentials.java
@@ -0,0 +1,8 @@
+package projector;
+
+public class Credentials {
+    public static final String DOMAIN = "localhost:8080";
+    public static final String WWW_DOMAIN = DOMAIN;
+    public static final String BASE_URL = "http://" + WWW_DOMAIN; // if it's with https then on other devices will not work properly
+    public static final String BASE_URL_S = "http://" + WWW_DOMAIN;
+}
