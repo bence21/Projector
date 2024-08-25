d46bfcb016ab56e6d3f351000a9876d09599e76c
Bako Bencze <bence@gineri.com>
2024-08-25 13:04:31 +0300

    Desktop - 3.5.8 - 82 create.bat

--- Projector-server/aPublic_folder/create.bat
+++ Projector-server/aPublic_folder/create.bat
@@ -29,7 +29,7 @@ echo "Directory and subdirectory created, file copied successfully."
 
 endlocal
 
-powershell Compress-Archive -Path "C:\workspace\Projector\Projector\Projector-server\aPublic_folder\update\*" -DestinationPath "projectorUpdate81.zip" -Force
+powershell Compress-Archive -Path "C:\workspace\Projector\Projector\Projector-server\aPublic_folder\update\*" -DestinationPath "projectorUpdate82.zip" -Force
 
 echo "Update tried to create!"
 
