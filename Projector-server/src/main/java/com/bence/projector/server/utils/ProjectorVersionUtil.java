1c17aef543a9072b30b8b8871b28efc908cd92b7
Bako Bencze <bence@gineri.com>
2024-12-20 17:40:46 +0200

    Desktop - 3.6.0 - 91

--- Projector-server/src/main/java/com/bence/projector/server/utils/ProjectorVersionUtil.java
+++ Projector-server/src/main/java/com/bence/projector/server/utils/ProjectorVersionUtil.java
@@ -9,10 +9,10 @@ import java.util.Date;
 public class ProjectorVersionUtil {
     public static void createNewProjectorVersion(ProjectorVersionRepository projectorVersionRepository) {
         ProjectorVersion projectorVersion = new ProjectorVersion();
-        projectorVersion.setVersion("3.5.16");
+        projectorVersion.setVersion("3.6.0");
         projectorVersion.setCreatedDate(new Date());
         projectorVersion.setDescription("Performance improvements and bug fixes.");
-        projectorVersion.setVersionId(90);
+        projectorVersion.setVersionId(91);
         projectorVersionRepository.save(projectorVersion);
     }
 }
