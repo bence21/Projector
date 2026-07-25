b1cf752710554790000f123e452f0882a3c260ee
Bako Bencze <bence@gineri.com>
2026-07-25 20:28:08 +0300

    Desktop - 3.7.6 - 103

--- Projector-server/aPublic_folder/create.bat
+++ Projector-server/aPublic_folder/create.bat
@@ -38,7 +38,7 @@ echo "Directory and subdirectory created, file copied successfully."
 
 endlocal
 
-powershell Compress-Archive -Path "C:\workspace\Projector\Projector\Projector-server\aPublic_folder\update\*" -DestinationPath "%~dp0projectorUpdate102.zip" -Force
+powershell Compress-Archive -Path "C:\workspace\Projector\Projector\Projector-server\aPublic_folder\update\*" -DestinationPath "%~dp0projectorUpdate103.zip" -Force
 
 echo "Update tried to create!"
 
--- Projector-server/src/main/java/com/bence/projector/server/utils/ProjectorVersionUtil.java
+++ Projector-server/src/main/java/com/bence/projector/server/utils/ProjectorVersionUtil.java
@@ -9,10 +9,10 @@ import java.util.Date;
 public class ProjectorVersionUtil {
     public static void createNewProjectorVersion(ProjectorVersionRepository projectorVersionRepository) {
         ProjectorVersion projectorVersion = new ProjectorVersion();
-        projectorVersion.setVersion("3.7.5");
+        projectorVersion.setVersion("3.7.6");
         projectorVersion.setCreatedDate(new Date());
         projectorVersion.setDescription("Performance improvements and bug fixes.");
-        projectorVersion.setVersionId(102);
+        projectorVersion.setVersionId(103);
         projectorVersionRepository.save(projectorVersion);
     }
 }
